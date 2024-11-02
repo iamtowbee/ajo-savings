293990cc74df351c081f8814ce4f192b9c1807f8
Oluwatobi Oluyede <oluwatobioluyede4@gmail.com>
2024-11-02 10:36:54 +0100

    Build savings group main page laypout

---
+++ components/SavingsGroup.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+interface Member {
+  id: number;
+  name: string;
+  tier: number;
+  amount: number;
+  interest: number;
+}
+
+const SavingsGroup = () => {
+  return <div>SavingsGroup</div>;
+};
+
+export default SavingsGroup;
